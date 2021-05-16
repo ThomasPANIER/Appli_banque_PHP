b0602f972634cba519ece53aff10f991003cbc09
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-05-16 22:27:45 +0200

    template ok

---
+++ layout/footer.php
@@ -0,0 +1,7 @@
+
+<footer class="bg-secondary text-center">
+    <section class="row justify-content-center">
+        <a class="p-3" href="index.php" title="Page d'accueil"><img id="logo_footer" alt="logo BPR" src="img/logo_BPR_light.png"></a>
+        <p>BPR® La banque proche de ses clients et de leur porte monnaie</p>
+    </section>
+</footer>
